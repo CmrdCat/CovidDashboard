0d73d89490f8b61b19a27add7e4cf08d4bb4b843
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2020-12-16 01:17:40 +0300

    add: countries with cases with scroll

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import 'bootstrap';
 import globalCasesTotal from './modules/globalCasesTotal/globalCasesTotal';
+import casesByCountry from './modules/casesByCountry/casesByCountry';
+
+const properties = {
+  country: 'all' || 'Belarus',
+  type: 'recovered' || 'cases' || 'death',
+  duration: 'all' || 'lastDay',
+  count: 'absolute' || 'on100',
+};
 
 async function init() {
   const url = `https://api.covid19api.com/summary`;
   const res = await fetch(url);
   const globalData = await res.json();
   globalCasesTotal(globalData);
+  casesByCountry(globalData, properties);
 }
 
 init();
-
-// let properties = {
-//   country: 'all' || 'Belarus',
-//   type: 'recovered' || 'cases' || 'death',
-//   duration: 'all' || 'lastDay',
-//   count: 'absolute' || 'on100',
-// }
