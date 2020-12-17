3d82357da637de98de548f2c7219d58f70f7e6f7
RomanFilipenia <biologg@mail.ru>
2020-12-17 21:25:43 +0300

    fix: filds hide on click button and appear on next click

--- src/modules/fullScreenButton/fullScreen.js
+++ src/modules/fullScreenButton/fullScreen.js
@@ -4,10 +4,26 @@ export default async function fullScreen() {
       const button = document.createElement('button');
       button.id = 'sweep';
       element.append(button);
+      document.getElementById('sweep').addEventListener('click', () => {
+        element.classList.add('fullScreen');
+        document.querySelectorAll('section').forEach((key) => {
+          if (!key.classList.value.includes('fullScreen')) {
+            if (!key.hidden) {
+              const obj = key;
+              obj.hidden = true;
+            } else {
+              const obj = key;
+              obj.classList.remove('fullScreen');
+              obj.hidden = false;
+            }
+          }
+        });
+      });
     });
     element.addEventListener('mouseleave', () => {
       const button = document.getElementById('sweep');
       element.removeChild(button);
+      element.classList.remove('fullScreen');
     });
   });
 }
