9088480c260824b20efc7fcb6a25de9affc9afb6
RomanFilipenia <biologg@mail.ru>
2020-12-17 22:42:07 +0300

    fix: correct add and delete class fullScreen

--- src/modules/fullScreenButton/fullScreen.js
+++ src/modules/fullScreenButton/fullScreen.js
@@ -5,25 +5,28 @@ export default async function fullScreen() {
       button.id = 'sweep';
       element.append(button);
       document.getElementById('sweep').addEventListener('click', () => {
-        element.classList.add('fullScreen');
-        document.querySelectorAll('section').forEach((key) => {
-          if (!key.classList.value.includes('fullScreen')) {
-            if (!key.hidden) {
+        if (!element.classList.value.includes('fullScreen')) {
+          element.classList.add('fullScreen');
+          document.querySelectorAll('section').forEach((key) => {
+            if (!key.classList.value.includes('fullScreen')) {
               const obj = key;
               obj.hidden = true;
-            } else {
+            }
+          });
+        } else {
+          document.querySelectorAll('section').forEach((key) => {
+            if (!key.classList.value.includes('fullScreen')) {
               const obj = key;
-              obj.classList.remove('fullScreen');
               obj.hidden = false;
             }
-          }
-        });
+          });
+          element.classList.remove('fullScreen');
+        }
       });
     });
     element.addEventListener('mouseleave', () => {
       const button = document.getElementById('sweep');
       element.removeChild(button);
-      element.classList.remove('fullScreen');
     });
   });
 }
