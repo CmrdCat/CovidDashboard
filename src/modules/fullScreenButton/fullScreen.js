9c19f5634eeed96cd334c868b062e664a920f34f
RomanFilipenia <biologg@mail.ru>
2020-12-19 14:11:22 +0300

    add:svg files,fix: correct sweep filds

--- src/modules/fullScreenButton/fullScreen.js
+++ src/modules/fullScreenButton/fullScreen.js
@@ -3,10 +3,19 @@ export default async function fullScreen() {
     element.addEventListener('mouseenter', () => {
       const button = document.createElement('button');
       button.id = 'sweep';
+      const img = document.createElement('img');
+      if (!element.classList.value.includes('fullScreen')) {
+        img.src = '../../img/arrows-fullscreen.svg';
+      } else {
+        img.src = '../../img/arrows-angle-contract.svg';
+      }
+      img.id = 'img';
       element.append(button);
+      button.append(img);
       document.getElementById('sweep').addEventListener('click', () => {
         if (!element.classList.value.includes('fullScreen')) {
           element.classList.add('fullScreen');
+          img.src = '../../img/arrows-angle-contract.svg';
           document.querySelectorAll('section').forEach((key) => {
             if (!key.classList.value.includes('fullScreen')) {
               const obj = key;
@@ -26,6 +35,8 @@ export default async function fullScreen() {
     });
     element.addEventListener('mouseleave', () => {
       const button = document.getElementById('sweep');
+      const img = document.getElementById('img');
+      button.removeChild(img);
       element.removeChild(button);
     });
   });
