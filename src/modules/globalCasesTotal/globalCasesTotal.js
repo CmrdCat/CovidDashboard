47d203522cf6b406d04161839d156f03c0f61ccc
CmrdCat <63845158+CmrdCat@users.noreply.github.com>
2020-12-22 12:32:12 +0300

    feat: cases by country corrected send information to configuration

--- src/modules/globalCasesTotal/globalCasesTotal.js
+++ src/modules/globalCasesTotal/globalCasesTotal.js
@@ -1,5 +1,6 @@
 export default async function globalCasesTotal(globalData) {
   const container = document.getElementById('global-cases');
+  container.innerText = '';
   const message = document.createElement('div');
   const totalConfirmed = globalData.Global.TotalConfirmed;
   const totalConfirmedWithSpaces = `${totalConfirmed}`
